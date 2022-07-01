1be264a727cfaa6da5899be44fe11649f39945bc
your_name <your_email@example.com>
2022-07-01 14:49:26 +0900

    계산기 기능, 경로 및 디렉토리 이름 변경

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express') // require로 express 모듈을 가져온다.
 var app = express() // 가져온 모듈을 app이라는 변수로 사용한다.
 var bodyParser = require('body-parser') //미들웨어에서 파싱해주는 기능을 수행
 
-var router = require('./routers/router') //Express 모듈의 Router : 기능들을 모듈별로 나눌 수 있음(모듈별 분리)
+var router = require('./router') //Express 모듈의 Router : 기능들을 모듈별로 나눌 수 있음(모듈별 분리)
 
 //연결을 위한 listening을 시작(listening 이벤트 발생)
 app.listen(8008, function() { // 8008 포트로 서버 확인
